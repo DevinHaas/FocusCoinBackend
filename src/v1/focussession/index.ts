ad0a0f1804d95ca74840f142650836b1114cca00
Karim Touhami <IKT360%school>
2023-11-14 23:06:18 +0100

    Focus-Session controller
    - Added calculate-reward endpoint

--- src/v1/focussession/index.ts
+++ src/v1/focussession/index.ts
@@ -3,6 +3,34 @@ import {prisma} from "../../libs/prisma";
 import {SessionState} from "@prisma/client";
 
 const focusSessions = new Elysia({prefix: 'focus-sessions'})
+    .guard({
+        body: t.Object({
+            minutes: t.Number()
+        })
+    })
+    .post("/calculate-reward", async ({body}) => {
+        const {minutes} = body;
+
+        if (minutes > 90) {
+            return {
+                status: 400,
+                success: false,
+                message: "Minutes must be less than or equal to 90",
+            };
+        }
+
+        const coinsPerMinute = 0.25;
+
+        const rewardInCoins = Math.round(minutes * coinsPerMinute);
+
+        return {
+            success: true,
+            message: "Calculate reward in coins",
+            data: {
+                rewardInCoins,
+            },
+        };
+    })
     .get("/", async () => {
         const focusSessions = await prisma.focusSession.findMany();
         return {
