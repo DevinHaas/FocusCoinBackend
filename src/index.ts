10c767461d7d35046da6bd9b5ff30d533ebccf51
Karim Touhami <IKT360%school>
2023-11-15 21:28:11 +0100

    Swagger UI
    - Description and tags for endpoints to index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,38 @@ import chalk from 'chalk';
 import fs from 'fs';
 import v1 from "./v1/group";
 
-const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
+const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
 console.log(chalk.blue("    ______                      ______      _          ___    ____  ____"));
 console.log(chalk.blue("   / ____/___  _______  _______/ ____/___  (_)___     /   |  / __ \\/  _/"));
 console.log(chalk.blue("  / /_  / __ \\/ ___/ / / / ___/ /   / __ \\/ / __ \\   / /| | / /_/ // /"));
 console.log(chalk.blue(" / __/ / /_/ / /__/ /_/ (__  ) /___/ /_/ / / / / /  / ___ |/ ____// /"));
 console.log(chalk.blue("/_/    \\____/\\___/\\__,_/____/\\____/\\____/_/_/ /_/  /_/  |_/_/   /___/"));
-console.log("                                                                        ");
+console.log("");
 console.log(chalk.blue(`FocusCoin API | https://focuscoinapp.com/ | Version: ${packageJson.version}`));
 console.log("");
 
 const app = new Elysia()
     // @ts-ignore
     .use(swagger({
-        path: "/v1/swagger"
+        path: "/v1/swagger",
+        documentation: {
+            info: {
+                title: 'FocusCoin API Documentation',
+                version: '1.0.0'
+            },
+            tags: [
+                { name: 'App', description: 'General endpoints' },
+                { name: 'User', description: 'User endpoints' },
+                { name: 'Focus-session', description: 'Focus-session endpoints' }
+            ]
+        }
     }))
-    .get("/", () => "FocusCoin API is Running...")
+    .get("/", () => "FocusCoin API is Running...", {
+        detail: {
+            tags: ['App']
+        }
+    })
     .use(v1)
     .listen(3000);
 
