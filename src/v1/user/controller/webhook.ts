6365f8daa7359cc682a8a2f19fbb02272d84a48c
Karim Touhami <IKT360%school>
2024-02-11 20:14:29 +0100

    FEAT: Add webhook to sync user
    - webhooks can be received but headers are empty

--- src/v1/user/controller/webhook.ts
+++ src/v1/user/controller/webhook.ts
@@ -1,46 +1,57 @@
-import {Elysia} from "elysia";
-import {Webhook} from "svix";
+import {Elysia} from 'elysia';
+import {Webhook} from 'svix';
 
-type EventType = "user.created" | "user.updated" | "user.deleted";
+type EventType = 'user.created' | 'user.updated' | 'user.deleted';
 
 type Event = {
     data: Record<string, string | number>;
-    object: "event";
+    object: 'event';
     type: EventType;
 };
 
-const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
+const webhookSecret = process.env.WEBHOOK_SECRET;
+
+if (!webhookSecret) {
+    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
+}
 
 const webhook = new Elysia()
-    .post("/api/webhooks", async ({set, body}) => {
+    .post('/api/webhooks', async ({set, body}) => {
 
-            const headers = set.headers;
-            const payload: Buffer = body as Buffer;
+            // Get the headers
+            console.log('Headers: ' + JSON.stringify(set));
+            const headers: any = set.headers;
 
-            const svixId = headers["svix-id"];
-            const svixTimestamp = headers["svix-timestamp"];
-            const svixSignature = headers["svix-signature"];
+            // Get the body
+            console.log('Body: ' + JSON.stringify(body));
+            const payload = JSON.stringify(body);
+
+            const svixId = headers['svix-id'];
+            const svixTimestamp = headers['svix-timestamp'];
+            const svixSignature = headers['svix-signature'];
 
             if (!svixId || !svixTimestamp || !svixSignature) {
                 return {
                     status: 400,
                     success: false,
-                    message: "Error occurred -- no svix headers"
+                    message: 'Error occurred -- no svix headers'
                 }
             }
 
+            // Create a new Svix instance with your secret.
             const wh = new Webhook(webhookSecret);
 
             let evt: Event | null = null;
 
+            // Verify the payload with the headers
             try {
                 evt = wh.verify(payload, {
-                    "svix-id": svixId,
-                    "svix-timestamp": svixTimestamp,
-                    "svix-signature": svixSignature,
+                    'svix-id': svixId,
+                    'svix-timestamp': svixTimestamp,
+                    'svix-signature': svixSignature,
                 }) as Event;
             } catch (err: any) {
-                console.log("Webhook failed to verify. Error:", err.message);
+                console.log('Webhook failed to verify. Error:', err.message);
                 return {
                     status: 400,
                     success: false,
@@ -48,16 +59,17 @@ const webhook = new Elysia()
                 }
             }
 
+            // Get the ID and type
             const {id} = evt.data;
             const eventType = evt.type;
 
             console.log(`Webhook with an ID of ${id} and type of ${eventType}`);
-            console.log("Webhook body:", evt.data);
+            console.log('Webhook body:', evt.data);
 
             return {
                 status: 200,
                 success: true,
-                message: "Webhook received"
+                message: 'Webhook received'
             }
         },
         {
